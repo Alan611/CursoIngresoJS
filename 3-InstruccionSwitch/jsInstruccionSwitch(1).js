f5396b99d2816d06d36129ba9c210c9640e9684c
Alan611 <Alan611@users.noreply.github.com>
2018-02-10 05:06:21 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,16 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-
+switch(mesDelAño)
+{
+    case "Enero": alert("Que Comiences Bien el Año!!!");
+    break;
+    case "Marzo": alert("A Clases!!!");
+    break;
+    case "Julio": alert("Se Vienen las Vacaciones!!!");
+    break;
+    case "Diciembre": alert ("Felices Fiestas!!!");
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -5,8 +5,24 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
-	
-
-
-
+	switch (mesDelAño)
+    {
+    case "Enero": 
+    case "Marzo": 
+    case "Mayo":
+    case "Julio": 
+    case "Agosto": 
+    case "Octubre":
+    case "Diciembre":
+    alert("Este Mes Tiene 31 Días");
+    break;
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+    alert("Este Mes Tiene 30 Días");
+    break;
+    case "Febrero": alert("Este Mes Tiene 29 Días");
+    break;
+    }
 }//FIN DE LA FUNCIÓN
